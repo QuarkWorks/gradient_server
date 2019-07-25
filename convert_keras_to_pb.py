892062556efdcb465136dffb6bb50ed1cccacb5d
bryan pratte <bryanpratte@gmail.com>
2019-07-25 14:02:51 -0700

    -

--- convert_keras_to_pb.py
+++ convert_keras_to_pb.py
@@ -27,20 +27,20 @@ with CustomObjectScope(
         {'relu6': ReLU, 'DepthwiseConv2D': DepthwiseConv2D}):
     nsfw_model = keras.models.load_model('./models/nsfw_mobilenet2.224x224.h5')
 
-#Save model
-# if not os.path.exists(MODEL_DIR):
-#     os.makedirs(MODEL_DIR)
-#     export_path = os.path.join(MODEL_DIR, VERSION)
-#     print('export_path = {}\n'.format(export_path))
-#
-#     tf.saved_model.simple_save(
-#         keras.backend.get_session(),
-#         export_path,
-#         inputs={'input_image': nsfw_model.input},
-#         outputs={t.name:t for t in nsfw_model.outputs})
-#
-#     print('\nModel saved to ' + MODEL_DIR)
-# else:
-#     print('\nExisting model found at ' + MODEL_DIR)
-#     print('\nDid not overwrite old model. Run the job again with a different location to store the model')
+Save model
+if not os.path.exists(MODEL_DIR):
+    os.makedirs(MODEL_DIR)
+    export_path = os.path.join(MODEL_DIR, VERSION)
+    print('export_path = {}\n'.format(export_path))
+
+    tf.saved_model.simple_save(
+        keras.backend.get_session(),
+        export_path,
+        inputs={'input_image': nsfw_model.input},
+        outputs={t.name:t for t in nsfw_model.outputs})
+
+    print('\nModel saved to ' + MODEL_DIR)
+else:
+    print('\nExisting model found at ' + MODEL_DIR)
+    print('\nDid not overwrite old model. Run the job again with a different location to store the model')
 
