d40bc04d3ea3062a580b5b5218a2fcca0c938a76
bryan pratte <bryanpratte@gmail.com>
2019-09-30 16:33:36 -0700

    1

--- test.py
+++ test.py
@@ -15,6 +15,40 @@ args = parser.parse_args()
 MODEL_DIR = args.MODEL_DIR
 VERSION = args.VERSION
 
+#Download Fashion MNIST dataset and split it for train and test
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+#Scale images
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+#Reshape array
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
+
+#Build Keras model
+model = keras.Sequential([
+  keras.layers.Conv2D(input_shape=(28,28,1), filters=8, kernel_size=3,
+                      strides=2, activation='relu', name='Conv1'),
+  keras.layers.Flatten(),
+  keras.layers.Dense(10, activation=tf.nn.softmax, name='Softmax')
+])
+
+model.summary()
+
+#Read the EPOCH value from environment variable
+epochs = int(os.getenv("EPOCHS",1))
+
+#Compile and fit
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=epochs)
+
+#Check accuracy
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('\nModel accuracy: {}'.format(test_acc))
 
 shutil.rmtree(MODEL_DIR)
 
