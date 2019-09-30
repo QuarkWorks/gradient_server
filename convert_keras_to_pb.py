d27c56002103a832e89e28e608871c77eccda77a
bryan pratte <bryanpratte@gmail.com>
2019-09-30 16:30:54 -0700

    1

--- convert_keras_to_pb.py
+++ convert_keras_to_pb.py
@@ -11,68 +11,63 @@ import shutil
 import os
 from os.path import expanduser, join
 
-#Parse input parameters
-parser = argparse.ArgumentParser(description='Fashion MNIST Keras Model')
-parser.add_argument('--modelPath', default='./storage/model', type=str, dest='MODEL_DIR', help='location to store the model artifacts')
-parser.add_argument('--version', type=str, dest='VERSION', default="1", help='model version')
-args = parser.parse_args()
 
-MODEL_DIR = args.MODEL_DIR
-VERSION = args.VERSION
+MODEL_DIR = './models'
+VERSION = '2'
 
 
 
 
-print(os.listdir('/storage'))
-
-print(os.listdir(MODEL_DIR))
-shutil.rmtree('/storage/model3')
-
-shutil.rmtree(MODEL_DIR)
-print(os.listdir('/storage'))
+# print(os.listdir('./storage'))
+#
+# print(os.listdir(MODEL_DIR))
+# shutil.rmtree('./storage/model3')
+#
+# shutil.rmtree(MODEL_DIR)
+# print(os.listdir('./storage'))
 
 #Save model
-if not os.path.exists(MODEL_DIR):
-    os.makedirs(MODEL_DIR)
-    export_path = os.path.join(MODEL_DIR, VERSION) + '.pb'
-    print('export_path = {}\n'.format(export_path))
-
-    shutil.copy('./storage/quant_nsfw_mobilenet.pb', export_path)
-
-
-    print('\nModel saved to ' + MODEL_DIR)
-else:
-    print('\nExisting model found at ' + MODEL_DIR)
-    print('\nDid not overwrite old model. Run the job again with a different location to store the model')
-
-
-print(os.listdir(MODEL_DIR))
-
-
-
-#/storage/quant_nsfw_mobilenet.pb
-# tf.logging.set_verbosity(tf.logging.ERROR)
-# print(tf.VERSION)
-# print(keras.__version__)
-#
-# with CustomObjectScope(
-#         {'relu6': ReLU, 'DepthwiseConv2D': DepthwiseConv2D}):
-#     nsfw_model = keras.models.load_model('./models/nsfw_mobilenet2.224x224.h5')
-#
-# #Save model
 # if not os.path.exists(MODEL_DIR):
 #     os.makedirs(MODEL_DIR)
-#     export_path = os.path.join(MODEL_DIR, VERSION)
+#     export_path = os.path.join(MODEL_DIR, VERSION) + '.pb'
 #     print('export_path = {}\n'.format(export_path))
 #
-#     tf.saved_model.simple_save(
-#         keras.backend.get_session(),
-#         export_path,
-#         inputs={'input_image': nsfw_model.input},
-#         outputs={t.name:t for t in nsfw_model.outputs})
+#     shutil.copy('./storage/quant_nsfw_mobilenet.pb', export_path)
+#
 #
 #     print('\nModel saved to ' + MODEL_DIR)
 # else:
 #     print('\nExisting model found at ' + MODEL_DIR)
 #     print('\nDid not overwrite old model. Run the job again with a different location to store the model')
+#
+#
+# print(os.listdir(MODEL_DIR))
+
+
+
+#/storage/quant_nsfw_mobilenet.pb
+tf.logging.set_verbosity(tf.logging.ERROR)
+print(tf.VERSION)
+print(keras.__version__)
+
+with CustomObjectScope(
+        {'relu6': ReLU, 'DepthwiseConv2D': DepthwiseConv2D}):
+    nsfw_model = keras.models.load_model('./models/nsfw_mobilenet2.224x224.h5')
+
+#Save model
+if not os.path.exists(MODEL_DIR):
+    os.makedirs(MODEL_DIR)
+    export_path = os.path.join(MODEL_DIR, VERSION)
+    print('export_path = {}\n'.format(export_path))
+
+    tf.saved_model.simple_save(
+        keras.backend.get_session(),
+        export_path,
+        inputs={'input_image': nsfw_model.input},
+        outputs={t.name:t for t in nsfw_model.outputs})
+
+    print('\nModel saved to ' + MODEL_DIR)
+else:
+    print('\nExisting model found at ' + MODEL_DIR)
+    print('\nDid not overwrite old model. Run the job again with a different location to store the model')
 
